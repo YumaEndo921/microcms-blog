71a22f259d2d2f422649603681d5c6313ce07544
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-09-20 18:10:22 +0900

    idを消してみる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,6 @@ export default defineNuxtConfig({
   routeRules: {
     "/": { ssr: true, prerender: true },
     "/test.vue": { ssr: true, prerender: true },
-    "/[id].vue": { ssr: true, prerender: true },
+    // "/[id].vue": { ssr: true, prerender: true },
   },
 });
