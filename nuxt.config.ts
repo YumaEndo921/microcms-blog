6e1f740321b9528661cc5f959d8c41160c05d16c
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-12-03 17:46:08 +0900

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       title: "mamelib | 初心者エンジニアの勉強ブログ",
       meta: [
         { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        // { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
           hid: "description",
           name: "description",
