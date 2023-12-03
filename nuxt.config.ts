d33be88fea26f30a934a16e246947a57101fbb3e
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-12-03 18:08:37 +0900

    no hide description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          hid: "description",
+          // hid: "description",
           name: "description",
           content: "未経験から転職してエンジニアになった人間の書き残しです。",
         },
